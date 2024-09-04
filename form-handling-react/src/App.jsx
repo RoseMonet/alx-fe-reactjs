282e579e9704b94d87224a599fb209d27c708488
Rose Owili <rosemonet61@gmail.com>
2024-09-04 12:18:47 +0300

    updated formikForm.js, RegistrationForm.jsx, App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/formikForm';
+import RegistrationForm from './components/RegistrationForm.jsx';
+import formikForm from './components/formikForm.js';
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
       <h2>Controlled Components Form</h2>
       <RegistrationForm />
       <h2>Formik Form</h2>
-      <FormikForm />
+      <formikForm />
     </div>
   );
 };
