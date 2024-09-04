51e04aca225f8fd07b067d51aafbf1ca1ae26b28
Rose Owili <rosemonet61@gmail.com>
2024-09-04 13:14:18 +0300

    created react-router-advanced

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<ProtectedRoute component={Profile} />} />
+        <Route path="/posts/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
