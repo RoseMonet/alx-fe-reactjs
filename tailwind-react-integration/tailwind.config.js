ad92f56e365778b4f1e4718a7e2760323fca9701
Rose Owili <rosemonet61@gmail.com>
2024-09-08 12:51:55 +0300

    updated tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,17 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Update the purge key to content for Vite
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Paths to all template files
+  darkMode: 'media', // or 'class' for manual control
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      // Add your variant extensions here if needed
+      backgroundColor: ['active'],
+      textColor: ['focus-visible'],
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
 };
