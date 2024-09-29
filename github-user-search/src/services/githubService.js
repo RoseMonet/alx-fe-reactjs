c8c4ce1a6b0ce063568b86519132af1e7d1b9e23
Rose Owili <rosemonet61@gmail.com>
2024-09-29 18:30:07 +0300

    updated githubService.jsx

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://api.github.com/users';
 
 export const fetchUserData = async (username, location, minRepos) => {
-  const query = `q=${username ? username : ''} ${location ? `location:${location}` : ''} ${minRepos ? `repos:>${minRepos}` : ''}`;
-  const response = await axios.get(`https://api.github.com/search/users?${query}`);
+  const q = `${username ? username : ''} ${location ? `location:${location}` : ''} ${minRepos ? `repos:>${minRepos}` : ''}`;
+  const response = await axios.get(`https://api.github.com/search/users?q`);
   return response.data.items;
 };
