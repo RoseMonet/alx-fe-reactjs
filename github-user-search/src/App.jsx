51ce228d6c3eed78db9c39481c56bb6f37879406
Rose Owili <rosemonet61@gmail.com>
2024-09-29 18:33:22 +0300

    updated App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React, { useState } from 'react';
 import Search from './components/Search';
 import { fetchUserData } from './services/githubService';
 
